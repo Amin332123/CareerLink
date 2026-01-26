4b84a531394d166a9e61baae8379585e0176570f
ihmaditaha <ihmaditaha1@gmail.com>
2026-01-26 09:00:44 +0100

    added fileUpload method to auth controller

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\Service\AuthService;
 
 class AuthController
@@ -33,20 +34,49 @@ class AuthController
         }
         $Authservice = new AuthService();
         $user = $Authservice->authenticate($email, $password);
-        if($user){
+        if ($user) {
             $_SESSION["role"] = $user->getRole();
             $_SESSION["user_id"] = $user->getId();
-            if($_SESSION["role"] == "admin"){
-            require_once "app\Views\public\Admin\Dashboard.php";
+            if ($_SESSION["role"] == "admin") {
+                require_once "app\Views\public\Admin\Dashboard.php";
             }
-            if($_SESSION["role"] == "candidate"){
-            require_once "app\Views\public\Candidate\Dashboard.php";
+            if ($_SESSION["role"] == "candidate") {
+                require_once "app\Views\public\Candidate\Dashboard.php";
             }
-            if($_SESSION["role"] == "recruiter"){
-            require_once "app\Views\public\Admin\Dashboard.php";
+            if ($_SESSION["role"] == "recruiter") {
+                require_once "app\Views\public\Admin\Dashboard.php";
             }
         }
     }
+
+    public function uploadImage($file)
+    {
+        $uploadDir = 'app/Views/public_assets/images/';
+        $targetFile = $uploadDir . basename($_FILES[$file]['name']);
+        $uploadOk = 1;
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+        $check = getimagesize($_FILES[$file]['tmp_name']);
+        if ($check !== false) {
+            $uploadOk = 1;
+        }else{
+            $uploadOk = 0;
+        }
+        if(file_exists($targetFile)){
+            $uploadOk = 0;
+        }
+        if($fileType!='jpg' && $fileType!='jpeg' && $fileType!='png'){
+            $uploadOk = 0;
+        }
+        if($uploadOk){
+            return false;
+        }else{
+            if(move_uploaded_file($_FILES[$file]['tmp_name'],$targetFile)){
+                return $targetFile;
+            }
+        }
+        return false;
+    }
+
     public function addCandidate()
     {
         $name = $_POST['name'];
@@ -54,7 +84,7 @@ class AuthController
         $password = $_POST['password'];
         $image = $_POST['image'];
         $skills = json_decode($_POST['skills']);
-        if ( empty($name) || empty($email) || empty($password)) {
+        if (empty($name) || empty($email) || empty($password)) {
             $error = "name,Email and Password are required.";
         }
         if (strlen($password) < 6) {
@@ -69,7 +99,7 @@ class AuthController
         }
         $Authservice = new AuthService();
         $user = $Authservice->addCandidate();
-        if($user){
+        if ($user) {
             require_once "app\Views\public\Auth\login.php";
         }
         echo "error register";
@@ -81,7 +111,7 @@ class AuthController
         $password = $_POST['password'];
         $companyName = $_POST['companyName'];
         $companyImage = $_POST['companyImage'];
-        if ( empty($name) || empty($email) || empty($password)) {
+        if (empty($name) || empty($email) || empty($password)) {
             $error = "name,Email and Password are required.";
         }
         if (strlen($password) < 6) {
@@ -96,7 +126,7 @@ class AuthController
         }
         $Authservice = new AuthService();
         $user = $Authservice->addRecruiter();
-        if($user){
+        if ($user) {
             require_once "app\Views\public\Auth\login.php";
         }
         echo "error register";
